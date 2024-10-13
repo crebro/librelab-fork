525ba6746ea11f797eb5fc5c91797b1cfbd732be
Creation Duwal <creationduwal@gmail.com>
2024-10-13 13:18:01 +0545

    add: an iupac quirk

---
+++ content/iupac-quirk-1.md
@@ -0,0 +1,5 @@
+### Important IUPAC QUIRK
+
+You need to know this.
+
+[IUPAC Naming Quirk](https://chemistry.stackexchange.com/questions/136753/shouldnt-this-compound-be-called-2-methyl-5-1-2-dimethylpropylnonane)
--- scripts/contentmap.js
+++ scripts/contentmap.js
@@ -4,4 +4,9 @@ const contentFiles = {
     file: "contributing.md",
     image: "./assets/contributing illustration.png",
   },
+  "iupac-quirk-1": {
+    title: "An IUPAC Quirk",
+    file: "iupac-quirk-1.md",
+    image: "./assets/iupac-quirk.png",
+  },
 };
